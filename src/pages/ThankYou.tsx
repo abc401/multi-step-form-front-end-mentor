8d56ab2062f9e134100b82816c8a0cb59a5d172c
Awab Ahmed <koibhinheen@gmail.com>
2023-07-08 07:25:35 -0700

    thankyou page

--- src/pages/ThankYou.tsx
+++ src/pages/ThankYou.tsx
@@ -1,7 +1,31 @@
 export default function ThankYou() {
   return (
-    <div>
-      Thank You
-    </div>
+    <>
+      <div
+        className="
+        bg-white
+        mb-4 sm:my-auto
+        p-4 sm:p-0
+        rounded-xl
+        min-w-[250px]
+        sm:max-w-[452px]
+      "
+      >
+        <div
+          className="
+          flex flex-col items-center text-center
+        "
+        >
+          <img
+            className="w-20"
+            src="/images/icon-thank-you.svg"
+            alt=""
+          />
+          <h1>Thank You!</h1>
+          <p>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.</p>
+        </div>
+      </div>
+      <div className="h-20 sm:hidden"></div>
+    </>
   )
 }
